3323e5301b11af53d98610484aa35b41912d0c74
DevTausif <kabirtausif610@gmail.com>
2024-08-29 00:10:40 +0600

    math and shorthand js

---
+++ Basic/number.js
@@ -0,0 +1,16 @@
+var orangePrice= 20;
+var chocolate = 0.5;
+
+console.log(typeof orangePrice, typeof chocolate);
+console.log(orangePrice, chocolate); 
+
+var appleChocolate = parseInt('20');
+var appleJuice = parseFloat('20.5');
+console.log(typeof appleChocolate, typeof appleJuice);
+console.log(appleChocolate, appleJuice);
+
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total.toFixed(1));
---
+++ Basic/shortHand.js
@@ -0,0 +1,10 @@
+var price = 35;
+// price = price+10;
+
+// this is shorthand
+price += 10;  
+
+price-=20;
+
+console.log(price);
+

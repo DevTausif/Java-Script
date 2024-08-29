b54d396f51a194d73164bbc84c05f0faab174435
DevTausif <kabirtausif610@gmail.com>
2024-08-29 23:24:07 +0600

    compare js

---
+++ compare.js
@@ -0,0 +1,14 @@
+// console.log(5>10);
+// console.log(5<10);
+// console.log(5==10);
+// console.log(10==10);
+// console.log(10 >= 10);
+// console.log(15 >= 10);
+console.log(15 == '15');
+console.log(15 === '15');
+// ................
+
+var mangoPrice = 250;
+var applePrice = 350;
+
+console.log(mangoPrice >= applePrice);

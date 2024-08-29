b948a50eac2296bab406b7ec7db45799d101b8b8
DevTausif <kabirtausif610@gmail.com>
2024-08-29 23:37:15 +0600

    conditional js

---
+++ conditional.js
@@ -0,0 +1,15 @@
+let age = 17;
+let withAdult = true;
+
+if(age >= 15){
+    console.log('You can go Inside Mela');
+    if(withAdult){
+        console.log('You Can go Inside The House');
+    }
+    else{
+        console.log('You can not go inside the mela');
+    }
+}
+else{
+    console.log('You can not go inside the mela');
+}

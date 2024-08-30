dfcbbf8a7b7544630dd2fcb974273a96eb1d3f41
DevTausif <kabirtausif610@gmail.com>
2024-08-30 22:30:06 +0600

    nested js

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money = 5;
+if(money >= 300){
+    console.log('Bro you are rich')
+}
+else{
+    if(money > 100){
+        console.log('You are middle class')
+    }
+    else{
+        if(money > 10){
+            console.log('Eat Banana');
+        }
+        else{
+            console.log('Work Hard and Make Money');
+        }
+    }
+}

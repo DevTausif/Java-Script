35acb19faed2196c76c5b169037150bb8348b30a
DevTausif <kabirtausif610@gmail.com>
2024-08-31 18:12:31 +0600

    set get index and length check on array

---
+++ Basic/Array/length-index-get-set.js
@@ -0,0 +1,9 @@
+// length
+const number = [1,2,3,5,6,8,9,10]
+console.log(number.length);
+console.log(number)
+// get element value by index
+console.log(number[5])
+// set element value by index
+number[5] = 1;
+console.log(number);
--- Basic/Array/store-array.js
+++ Basic/Array/store-array.js
@@ -1,2 +1,5 @@
 const numbers = [1,2,3,4,5]
 console.log(typeof numbers, numbers)
+console.log(numbers.length)
+numbers[0]=5
+console.log(numbers);

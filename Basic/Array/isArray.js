7ae40105afd08fff2c94b3b00975252fedcd0a3a
DevTausif <kabirtausif610@gmail.com>
2024-09-01 01:34:55 +0600

    join operator added

--- Basic/Array/isArray.js
+++ Basic/Array/isArray.js
@@ -1,9 +1,15 @@
 const number = [1,2,3,5,6,8]
 const nums = []
+const num = [1,2,3,5,6,8]
+let result = num.join(' ');
+
 const numbers = Array.isArray([1,2,3,6,5,4])
 
 const age = 20
 console.log(Array.isArray(number))
+console.log(number.slice(1,4))
+console.log(number.concat(num))
 console.log(Array.isArray(nums))
 console.log(Array.isArray(age))
 console.log(numbers)
+console.log(result)

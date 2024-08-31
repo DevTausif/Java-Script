4ea8c2124304ef62057b88c7191cd7e692df651d
DevTausif <kabirtausif610@gmail.com>
2024-09-01 01:23:15 +0600

    array added

--- Basic/Array/isArray.js
+++ Basic/Array/isArray.js
@@ -1,7 +1,9 @@
 const number = [1,2,3,5,6,8]
 const nums = []
+const numbers = Array.isArray([1,2,3,6,5,4])
 
 const age = 20
 console.log(Array.isArray(number))
 console.log(Array.isArray(nums))
 console.log(Array.isArray(age))
+console.log(numbers)

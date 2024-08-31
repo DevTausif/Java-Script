a13cd04b38ad84cca9bb730dcbd68e1f3b48fbd5
DevTausif <kabirtausif610@gmail.com>
2024-08-31 18:30:59 +0600

    shift unshift push pop array js

---
+++ Basic/Array/add-pop.js
@@ -0,0 +1,19 @@
+// Add element in array
+const number = [1,2,3,5,4,6]
+console.log(number)
+number.push(44)
+console.log(number)
+// remove element in array
+let del=number.pop() // Store deleted array element on variable
+console.log(number)
+console.log(del)
+
+// shift array
+let add = number.shift()  // Store shifted array element on variable
+console.log(number)
+console.log(add)
+
+// unshift array
+
+number.unshift(10) // Store unshift array 1st element
+console.log(number); 

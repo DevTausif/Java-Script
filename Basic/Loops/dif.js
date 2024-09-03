609b6c76da98b24ff35e46fe262cacb9591cbd78
DevTausif <kabirtausif610@gmail.com>
2024-09-04 01:47:39 +0600

    break and continue

---
+++ Basic/Loops/dif.js
@@ -0,0 +1,19 @@
+// for(let i = 51; i < 65; i+=2){
+//     console.log(i)
+// }
+
+// for(let i = 1; i <= 30; i++){
+//     if(i %  5 === 0){
+//         console.log(i)
+//     }
+// }
+// for(let i = 1; i <= 30; i++){
+//     if(i %  3 === 0 || i % 5 ===0){
+//         console.log(i)
+//     }
+// }
+for(let i = 1; i <= 30; i++){
+    if(i %  3 === 0 && i % 5 ===0){
+        console.log(i)
+    }
+}

90f8ba984e90c0718bee429cccffb0184deedc9f
DevTausif <kabirtausif610@gmail.com>
2024-09-05 02:09:37 +0600

    object

---
+++ Basic/String-Object/object.js
@@ -0,0 +1,16 @@
+const age = 21;
+const school = 'Ideal School'
+const isPassed =true;
+let isDev;
+const subjects = ['bng',' eng','phy','math']
+
+// object declare
+// Object is non primitive data-type 
+const sub = {
+    firstSub:'bng',
+    secondSub:'eng',
+    thirdSub:'phy',
+    fourthSub:'math'
+
+}
+console.log(typeof sub)

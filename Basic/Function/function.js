b762def632fa2d4033e07edf0469d17cbc52defa
DevTausif <kabirtausif610@gmail.com>
2024-09-06 01:03:09 +0600

    function js

---
+++ Basic/Function/function.js
@@ -0,0 +1,14 @@
+let price = 45;
+function fanOff(){
+    console.log('Off The Fan')
+    console.log('Walk Towards the Switch')
+    console.log('Click The Switch to off the fan')
+}
+function totalPrice(){
+    price = price + 100;
+    console.log(price)
+    
+}
+fanOff()
+totalPrice();
+

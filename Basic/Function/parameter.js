fed5817eff10ea079915112110ec25477c9176c2
DevTausif <kabirtausif610@gmail.com>
2024-09-06 01:28:06 +0600

    parameter js

---
+++ Basic/Function/parameter.js
@@ -0,0 +1,20 @@
+function eatRice(){
+    console.log('First Wash Your Hand')
+    console.log('Sit on the dining table')
+    console.log('Then serve rice yourself from the rice-bowl')
+    console.log('Then before you start eating, you say bismillah and start your lunch')
+}
+// eatRice()
+
+function square(number){
+    // console.log(number)
+    const final = number * number
+    console.log(final)
+}
+square(5)
+
+function  add(num1, num2, num3){
+    const sum = num1 + num2 + num3;
+    console.log(sum)
+}
+add(5,10,50)

9d4553445c320ac4d8e9900a2fbb674908d40395
DevTausif <kabirtausif610@gmail.com>
2024-09-06 01:50:28 +0600

    parameter

--- Basic/Function/parameter.js
+++ Basic/Function/parameter.js
@@ -12,9 +12,20 @@ function square(number){
     console.log(final)
 }
 square(5)
+const number = 25;
+square(number)
 
 function  add(num1, num2, num3){
     const sum = num1 + num2 + num3;
     console.log(sum)
 }
 add(5,10,50)
+
+// difference
+function difference(num1, num2){
+    const diff = num1 - num2
+    console.log('Difference of age is',diff)
+}
+const fatherAge = 50;
+const myAge = 26;
+difference(fatherAge, myAge)

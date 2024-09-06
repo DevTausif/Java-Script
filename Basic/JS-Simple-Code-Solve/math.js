f92664fd5ebbbb7b389ef33b2fccb2abbc956d3d
DevTausif <kabirtausif610@gmail.com>
2024-09-06 22:59:46 +0600

    mat js

---
+++ Basic/JS-Simple-Code-Solve/math.js
@@ -0,0 +1,13 @@
+const min = Math.min(45,7,123,500,65,-99);
+const max = Math.max(45,7,123,500,65,-99);
+// const pi = Math.PI(45,7,123,500,65,-99);
+console.log(min, max);
+console.log(Math.PI);
+console.log(Math.abs(-88));
+console.log(Math.round(1.2));
+console.log(Math.floor(2.99));
+console.log(Math.ceil(2.69856985));
+// console.log(Math.random()*10);
+
+const rand = Math.round(Math.random()*10);
+console.log(rand)

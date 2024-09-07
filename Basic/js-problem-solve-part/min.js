17a47ce3d8a33474043ba7c49ad96275a1737b6d
DevTausif <kabirtausif610@gmail.com>
2024-09-08 01:47:23 +0600

    min js

---
+++ Basic/js-problem-solve-part/min.js
@@ -0,0 +1,13 @@
+function minPrice(numbers){
+    let min = numbers[0];
+    for(const num of numbers){
+        if(num < min){
+            min = num;
+        }        
+    }
+    return min;
+}
+
+const prices = [20000, 16000, 50000, 100000, 30000, 35000];
+const finalPrice = minPrice(prices);
+console.log(finalPrice);

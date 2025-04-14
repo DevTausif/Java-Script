7efa60f1c86d071e52a3cf817ba77d32bcdb2cf4
DevTausif <kabirtausif610@gmail.com>
2025-04-14 12:25:29 +0600

    ternary operator

--- practice js/ternary.js
+++ practice js/ternary.js
@@ -4,5 +4,10 @@ age === 20 ? console.log('yes') : console.log('no')
 let price = 500
 const isLeader = true
 
-const finalPrice = price === 500 && isLeader == false ? console.log('hahaha') : price += 200
+const finalPrice = price === 500 && isLeader === false ? console.log('hahaha') : price += 200
 console.log(finalPrice)
+
+let price1 = 50;
+const leader2 = false;
+const result = leader2 === true ? price1 > 100 ? price1 = price1 / 2 : price1 = 0 : price1 += 1000
+console.log(result)

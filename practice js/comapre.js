e3727634e17399803628e149877188790cc044b0
DevTausif <kabirtausif610@gmail.com>
2025-04-14 12:01:22 +0600

    nested condition js

--- practice js/comapre.js
+++ practice js/comapre.js
@@ -8,3 +8,18 @@
 //     console.log('kkkk')
 //  }
 
+const price = 5000;
+if(price > 5000){
+    console.log('eat free')
+}
+else if(price === 5000){
+    const discount = price * 10 / 100;
+    if(discount === 500){
+        const discountAmount = price - discount
+        console.log(discountAmount)
+    }
+    // console.log(discount)
+}
+else{
+    console.log(price)
+}

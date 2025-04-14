2681cd4609529dcb51767bef176c695fd52587aa
DevTausif <kabirtausif610@gmail.com>
2025-04-14 12:18:47 +0600

    ternary

--- practice js/ternary.js
+++ practice js/ternary.js
@@ -1,2 +1,8 @@
-const age = 20 
+const age = 2
 age === 20 ? console.log('yes') : console.log('no')
+
+let price = 500
+const isLeader = true
+
+const finalPrice = price === 500 && isLeader == false ? console.log('hahaha') : price += 200
+console.log(finalPrice)

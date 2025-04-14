b363c18bdbcaee0ec720539dfe035e0a2c9f48f8
DevTausif <kabirtausif610@gmail.com>
2025-04-14 11:40:36 +0600

    compare js

---
+++ practice js/comapre.js
@@ -0,0 +1,10 @@
+// console.log(10 == '10')
+// console.log(10 === '10')
+// console.log(10 != '10')
+// console.log(10 !== '10')
+
+// let price = 600
+//  if(price >= 500){
+//     console.log('kkkk')
+//  }
+

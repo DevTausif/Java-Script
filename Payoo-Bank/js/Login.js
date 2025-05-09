f1d99f9eb18eac3b5dbe8ef59039347b3bf4cad8
DevTausif <kabirtausif610@gmail.com>
2025-05-09 17:37:16 +0600

    phone number and pin validation and home page connected through js

--- Payoo-Bank/js/Login.js
+++ Payoo-Bank/js/Login.js
@@ -1,6 +1,13 @@
-document.getElementById('login-btn').addEventListener('click',(event)=>{
+document.addEventListener('DOMContentLoaded',()=>{
+    document.getElementById('login-btn').addEventListener('click',(event)=>{
     event.preventDefault()
     const phn_num = document.getElementById('phone-number').value
-   
-    
+    const pin_num = document.getElementById('pin').value
+    if(phn_num.length === 11 && pin_num.length === 4){
+        window.location.href = 'home.html'
+    }
+    else{
+        alert('Invalid')
+    }    
+})
 })
